7be3f45fa522e9d684bffc346e349485a0893b63
David Choi <Choi@davids-mbp-5.attlocal.net>
2016-11-23 19:32:43 -0800

    able to implement snapshot

--- ios-final-project/CreateAccountViewController.swift
+++ ios-final-project/CreateAccountViewController.swift
@@ -30,17 +30,56 @@ class CreateAccountViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func checkCreateAccount() -> Bool {
+        var check = false
+        if firstName.text!.isEmpty {
+            print("Enter first name")
+            check = true
+        }
+        if lastName.text!.isEmpty {
+            print("Enter last name")
+            check = true
+        }
+        if userName.text!.isEmpty {
+            print("Enter  user-name")
+            check = true
+        }
+        if newPassword.text!.isEmpty {
+            print("Enter password")
+            check = true
+        }
+        if phoneNumber.text!.isEmpty {
+            print("Enter phone number")
+            check = true
+        }
+        if check {
+            return false
+        }
+        return true
+    }
+    
     
     @IBAction func CreateAccount(_ sender: Any) {
-        let alias = userName.text
-        
-        let user = FIRDatabase.database().reference(withPath: "UserName").child(alias!)
-        rootRef.child("allUsers").child(alias!).setValue(0)
-        
-        user.child("FirstName").setValue(firstName.text!)
-        user.child("LastName").setValue(lastName.text!)
-        user.child("Password").setValue(newPassword.text!)
-        user.child("phoneNumber").setValue(phoneNumber.text!)
+        if checkCreateAccount() {
+            let alias = userName.text
+            
+            self.rootRef.child("allUsers").observe(.value, with: {FIRDataSnapshot in
+                for child in FIRDataSnapshot.children {
+                    let name = child
+                    print(name)
+                }
+            })
+            
+            let user = FIRDatabase.database().reference(withPath: "UserName").child(alias!)
+            rootRef.child("allUsers").child(alias!).setValue(0)
+            
+            user.child("FirstName").setValue(firstName.text!)
+            user.child("LastName").setValue(lastName.text!)
+            user.child("Password").setValue(newPassword.text!)
+            user.child("phoneNumber").setValue(phoneNumber.text!)
+        } else {
+            print("missing stuff")
+        }
         
     }
 
