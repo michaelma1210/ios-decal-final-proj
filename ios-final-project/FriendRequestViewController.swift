6d4d7d8c98036aeb4736c646a977c058a6afa764
David Choi <Choi@Davids-MacBook-Pro-5.local>
2016-11-30 19:03:45 -0800

    up to date

--- ios-final-project/FriendRequestViewController.swift
+++ ios-final-project/FriendRequestViewController.swift
@@ -101,6 +101,7 @@ class FriendRequestViewController: UIViewController, UITableViewDataSource, UITa
             self.friendRequestList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
             
+            
         })
         
         return [notNowAction, acceptAction]
