e59dfa575a8d93eb3ce715a4759d99251b10fb80
Michael Ma <michaelma1210@gmail.com>
2016-12-01 02:38:02 -0800

    Added New Event on storyboard

---
+++ ios-final-project/NewEventViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewEventViewController.swift
+//  ios-final-project
+//
+//  Created by Michael Ma on 12/1/16.
+//  Copyright © 2016 Michael Ma. All rights reserved.
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
